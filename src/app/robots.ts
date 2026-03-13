8b5781cf3a1e3b7e771c989b58c4639f6d0c3e3b
Tiago Ribeiro <tiagoribeiro@iMac-2.fritz.box>
2026-03-13 18:18:24 +0100

    Fix sitemap: remove www to match canonical domain and avoid 'URL not allowed' errors

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.bonne-etoile.com/sitemap.xml',
+    sitemap: 'https://bonne-etoile.com/sitemap.xml',
   }
 }
